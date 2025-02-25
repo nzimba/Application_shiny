9b8aee183859244101b6944df8b397fa53f5bc08
nzimba <nzimbabeni@yahoo.com>
2025-02-25 13:47:27 +0100

    les packages suivants, je le pris dans le livre mastering Shiny for O'REILLY

---
+++ Packages/Bibliotheques.R
@@ -0,0 +1,25 @@
+# install.packages(c(
+#   "gapminder", "ggforce", "gh", "globals", "openintro", "profvis", 
+#   "RSQLite", "shiny", "shinycssloaders", "shinyFeedback", 
+#   "shinythemes", "testthat", "thematic", "tidyverse", "vroom", 
+#   "waiter", "xml2", "zeallot" 
+# ))
+
+library (gapminder)
+library (ggforce)
+library (gh) 
+library (globals) 
+library (openintro) 
+library (profvis)
+library (RSQLite) 
+library (shiny)
+library (shinycssloaders) 
+library (shinyFeedback)
+library (shinythemes)
+library (testthat)
+library (thematic) 
+library (tidyverse) 
+library (vroom)
+library (waiter) 
+library (xml2)
+library (zeallot) 
